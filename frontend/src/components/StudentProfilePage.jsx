c4c12f2ee8ea1ec5f914c0863c7810870de30a31
harsh <harshwardhany87@gmail.com>
2026-01-06 14:07:19 +0530

    add useEffect for system not crash

--- frontend/src/components/StudentProfilePage.jsx
+++ frontend/src/components/StudentProfilePage.jsx
@@ -1,11 +1,28 @@
+import {useState} from 'react';
+import {useEffect} from 'react';
+import axios from 'axios';
 export default function StudentProfilePage({newUserData}){
+    const [profileData,setProfileData]=useState('');
+
+    useEffect(()=>{
+        const fetch=async()=>{
+            try{
+    const response=await axios.get('https://event-managaement-system-backend.onrender.com/api/Student/CheckStudent',{withCredentials:true});
+    setProfileData(response.data.data);
+            }catch(err){
+                console.log("profile not found",err);
+            }
+        };
+        fetch();
+    },[])
+    console.log(newUserData.rollNo);
     return(
         <>
         <h1>Welcome to your profile </h1>
-        <p>{newUserData?.rollNo}</p>
-        <p>{newUserData?.section}</p>
-        <p>{newUserData?.semester}</p>
-        <p>{newUserData?.branch}</p>
+        <p>{profileData?.rollNo}</p>
+        <p>{profileData?.section}</p>
+        <p>{profileData?.semester}</p>
+        <p>{profileData?.branch}</p>
         </>
     );
 }
